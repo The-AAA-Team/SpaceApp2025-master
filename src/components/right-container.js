9fbb8e6450045f598537fa0b2112b6f8fbf5b4c9
Brandon D. <alyssajoseph415@gmail.com>
2025-10-04 15:41:46 -0700

    Fixed the coloring and added links to the text

--- src/components/right-container.js
+++ src/components/right-container.js
@@ -5,9 +5,11 @@ function RightContainer(){
     return(
         <div class="right-container">
             {publicationsData.slice(0,10).map(publication => (
-            <div class="right-rows">
-                {publication.Title}
-            </div>
+                <div class="right-rows">
+                    <a href="">
+                        {publication.Title}
+                    </a>
+                </div>
             ))}
         </div>
     );
