eda14d6e37b6954103518245f03473ab8cc9b5f6
Brandon D. <alyssajoseph415@gmail.com>
2025-10-04 12:15:43 -0700

    Added a right container

---
+++ src/components/right-container.js
@@ -0,0 +1,40 @@
+import './right-container.css'
+
+function RightContainer(){
+    return(
+        <div class="right-container">
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+            <div class="right-rows">
+                Real
+            </div>
+        </div>
+    );
+}
+
+export default RightContainer;
