c5a57e8da5aedae1fd736b9d045acb2e07c83af7
Brandon D. <alyssajoseph415@gmail.com>
2025-10-04 16:30:32 -0700

    Making a placeholder for the nodes

--- src/components/left-container.js
+++ src/components/left-container.js
@@ -1,9 +1,10 @@
 import './left-container.css'
+import KnowledgeGraph from './knowledgegraph.js'
 
 function LeftContainer(){
     return(
         <div class="left-container">
-            stuffs
+            <KnowledgeGraph />
         </div>
     );
 }
