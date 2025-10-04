80633cf2da5ea9447ad2c7a128e34e608930880e
Brandon D. <alyssajoseph415@gmail.com>
2025-10-04 16:39:36 -0700

    Added the knowledge graph placeholder to the right place

--- src/components/knowledgegraph.js
+++ src/components/knowledgegraph.js
@@ -4,7 +4,7 @@ function KnowledgeGraph(){
     return(
         <div class="knowledge-graph-main">
             <div class="knowledge-graph-content">
-                Test
+                
             </div>
         </div>
     )
