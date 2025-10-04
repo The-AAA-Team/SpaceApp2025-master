92d6a69d4ae54997ee4cf8e550365785cf5813c3
Megan Ciraulo <ciraulo.megan@gmail.com>
2025-10-04 11:31:59 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Footer from './components/footer.js'
+import Navbar from './components/navbar.js'
 
 function App() {
   return (
     <div className="App">
       <Footer />
+      <Navbar />
     </div>
   );
 }
