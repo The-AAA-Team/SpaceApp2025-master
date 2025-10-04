3b681d588dd0300caed41f7d78c34062260e3195
Brandon D. <alyssajoseph415@gmail.com>
2025-10-04 14:37:01 -0700

    Fixed the right content not working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 
 import Footer from './components/footer.js'
 import Navbar from './components/navbar.js'
-import RightContainer from './components/right-container.js'
+// import RightContainer from './components/right-container.js'
+import Contents from './components/contents.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <RightContainer />
+      <Contents />
       <Footer />
     </div>
   );
